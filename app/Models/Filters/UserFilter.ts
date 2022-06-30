ccc94e825d7fdadea9d57568c900112d9fa67a72
Gabriel <gabriel.mena.jofre@gmail.com>
2022-06-29 23:36:52 -0400

    hola

--- app/Models/Filters/UserFilter.ts
+++ app/Models/Filters/UserFilter.ts
@@ -5,12 +5,9 @@ import Usuario from 'App/Models/Users'
 export default class UsuarioFilter extends BaseModelFilter {
   public $query: ModelQueryBuilderContract<typeof Usuario, Usuario>
 
-  public q(search: string): void {
+  q(search: string) {
     this.$query.where((builder) => {
-      builder
-        .whereRaw(`upper("id") LIKE '%' || upper(?) || '%'`, [search])
-        .orWhereRaw(`upper("email") LIKE '%' || upper(?) || '%'`, [search])
-        .orWhereRaw(`upper("name") LIKE '%' || upper(?) || '%'`, [search])
+      builder.where('name', 'LIKE', `%${search}%`).orWhere('id', 'LIKE', `%${search}%`)
     })
   }
 }
