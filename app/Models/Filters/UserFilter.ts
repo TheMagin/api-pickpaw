c76b02bf00973d9e6da915cb34ccfa38e08f9f45
Gabriel <gabriel.mena.jofre@gmail.com>
2022-06-29 20:05:36 -0400

    make it better

--- app/Models/Filters/UserFilter.ts
+++ app/Models/Filters/UserFilter.ts
@@ -1,6 +1,6 @@
 import { BaseModelFilter } from '@ioc:Adonis/Addons/LucidFilter'
 import { ModelQueryBuilderContract } from '@ioc:Adonis/Lucid/Orm'
-import Usuario from 'App/Models/users'
+import Usuario from 'App/Models/Users'
 
 export default class UsuarioFilter extends BaseModelFilter {
   public $query: ModelQueryBuilderContract<typeof Usuario, Usuario>
