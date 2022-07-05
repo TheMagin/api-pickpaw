e92d7e569a59c0400daf0fc972b8ca6153f62d59
Gabriel <gabriel.mena.jofre@gmail.com>
2022-07-05 09:45:31 -0400

    DAS

---
+++ database/migrations/1657028527822_like_posts.ts
@@ -0,0 +1,32 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'like_posts'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary
+      table.string('title').notNullable()
+      table.string('description').notNullable()
+
+      table.string('status').notNullable()
+      table.boolean('post_state').notNullable()
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table.integer('post_id').unsigned().references('id').inTable('posts').onDelete('CASCADE')
+      table.integer('pet_id').unsigned().references('id').inTable('pets').onDelete('CASCADE')
+      table
+        .integer('type_post_id')
+        .unsigned()
+        .references('id')
+        .inTable('type_posts')
+        .onDelete('CASCADE')
+
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').defaultTo(this.now())
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
