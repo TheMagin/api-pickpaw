633faa62b446db342b4981032f95c6cac5344a4c
Gabriel <gabriel.mena.jofre@gmail.com>
2022-07-05 09:57:04 -0400

    DAS

---
+++ app/Models/Post.ts
@@ -0,0 +1,37 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Post extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public description: string
+
+  @column()
+  public date_expire: string
+
+  @column()
+  public status: string
+
+  @column()
+  public post_state: boolean
+
+  @column()
+  public user_id: number
+
+  @column()
+  public pet_id: number
+
+  @column()
+  public type_post_id: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
