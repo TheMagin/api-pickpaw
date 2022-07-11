3dcace184a225d5cb91a39d24afa9546ea7cd97b
Gabriel <gabriel.mena.jofre@gmail.com>
2022-07-10 23:32:39 -0400

    Asdfdsa

--- config/ally.ts
+++ config/ally.ts
@@ -27,7 +27,7 @@ const allyConfig: AllyConfig = {
     driver: 'google',
     clientId: Env.get('GOOGLE_CLIENT_ID'),
     clientSecret: Env.get('GOOGLE_CLIENT_SECRET'),
-    callbackUrl: 'https://api-pickpaw.herokuapp.com/google/callback',
+    callbackUrl: 'http://api-pickpaw.herokuapp.com/google/callback',
   },
 }
 
